f672a336063c2bad127c5857f05b487424f24683
Prabath udayanga <prabhathudayanga1998@gmail>
2022-04-18 22:36:48 +0530

    Add project

---
+++ lib/screens/home.dart
@@ -0,0 +1,36 @@
+import 'package:bmiapp/constants/app_constants.dart';
+import 'package:flutter/material.dart';
+import 'package:bmiapp/widgets/left_bar.dart';
+import 'package:bmiapp/widgets/right_bar.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'BMI Calculator',
+          style: TextStyle(color: accentHexColor, fontWeight: FontWeight.w300),
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        centerTitle: true,
+      ),
+      backgroundColor: mainHexColor,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
